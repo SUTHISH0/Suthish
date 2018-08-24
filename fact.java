965cc991f24d6eb89883226e9de8052b2353aced
SUTHISH0 <42672810+SUTHISH0@users.noreply.github.com>
2018-08-24 17:22:27 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.*; 
+class fact
+{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=1;
+    for(int i=1;i<=a;i++)
+    {
+        b=b*i;
+    }
+    System.out.print(b);
+}
+}
